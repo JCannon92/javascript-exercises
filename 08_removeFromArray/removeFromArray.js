02930f38b1e9ab98c388bdf7b65148c0439f2f11
John Cannon <92427288+JCannon92@users.noreply.github.com>
2025-11-19 16:25:31 +1030

    Complete exercise 07

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...nums) {
+    return array.filter(value => !nums.includes(value));
 };
+
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
