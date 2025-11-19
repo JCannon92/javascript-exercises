f46d13f1d256e700734d2523d3af72d2cf8f3c88
John Cannon <92427288+JCannon92@users.noreply.github.com>
2025-11-19 15:50:09 +1030

    Complete exercise 06.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let repeatedString = '';
 
+    if (num < 0) {
+        return 'ERROR';
+    }
+
+    for (let i = 1; i <= num; i++) {
+        repeatedString = repeatedString + string;
+    }
+
+    return repeatedString;
 };
 
 // Do not edit below this line
