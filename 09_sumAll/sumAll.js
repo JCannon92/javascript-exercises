c4f6bdba42358d978b2ca2090ebdd3e7874b81d7
John Cannon <92427288+JCannon92@users.noreply.github.com>
2025-11-19 16:57:37 +1030

    Complete exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+ 
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+        return 'ERROR';
+    } 
+    let small = a <= b ? a : b;
+    let big = a > b ? a : b;
+    let arr = [];
 
+    for (small; small <= big; small++) {
+        arr.push(small);
+    }
+
+    return arr.reduce((total, current) => total + current);   
 };
 
 // Do not edit below this line
