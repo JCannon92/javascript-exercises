59d52871d0df428e733aae926823c22edea1508c
John Cannon <92427288+JCannon92@users.noreply.github.com>
2025-11-25 12:41:07 +1030

    Complete exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+let findTheOldest = function(arr) {
+    let ageArr = arr.map(obj => {
+        if (obj.yearOfDeath) {
+            obj.age = obj.yearOfDeath - obj.yearOfBirth;
+        } else {
+            obj.age = new Date().getFullYear() - obj.yearOfBirth;
+        }
+        return obj;
+    });
+    
+    return ageArr.sort((a, b) => b.age - a.age)[0];
 };
 
 // Do not edit below this line
