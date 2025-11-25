a7d5e5a885eda0f9b3840f340cc05f0214b8dfb2
John Cannon <92427288+JCannon92@users.noreply.github.com>
2025-11-25 11:54:17 +1030

    Complete exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const cleanString = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase()
+    
+    return cleanString === [...cleanString].reverse().join('');
 };
 
 // Do not edit below this line
