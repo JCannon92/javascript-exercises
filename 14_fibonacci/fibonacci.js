ee1332a25e38faa245b47f37258b8a18488e1227
John Cannon <92427288+JCannon92@users.noreply.github.com>
2025-11-25 12:06:17 +1030

    Complete exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    //add special cases
+    num = Number(num);
+    if (num === 0 ) return 0;
+    if (num < 0) return 'OOPS';
 
+    const arr = [1, 1];
+    for (let i = 2; i <= num; i++) {
+        arr.push(arr[i-1] + arr[i-2]);
+    }
+
+    return arr[num - 1];
 };
 
 // Do not edit below this line
